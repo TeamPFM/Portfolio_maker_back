424ddea858d428dbd7ef3483f76bf7df9b3fc5be
cslee <cslee0034@gmail.com>
2023-02-16 22:57:45 +0900

    chore: Add swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,8 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { PickType } from '@nestjs/swagger';
+import { UsersEntity } from '../entities/users.entitiy';
 
-export class CreateUsersDto {
-  @IsEmail()
-  @IsNotEmpty({ message: '이메일은 비워져 있을 수 없습니다.' })
-  email: string;
-
-  @IsString()
-  @IsNotEmpty({ message: '이은 비워져 있을 수 없습니다.' })
-  name: string;
-
-  @IsString()
-  @IsNotEmpty({ message: '비밀번호는 비워져 있을 수 없습니다.' })
-  password: string;
-}
+export class CreateUsersDto extends PickType(UsersEntity, [
+  'email',
+  'name',
+  'password',
+] as const) {}
