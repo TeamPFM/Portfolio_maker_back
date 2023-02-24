9f89f91468002eb014b0489e34c0a7ea1e2cd0ca
cslee <cslee0034@gmail.com>
2023-02-24 16:29:39 +0900

    fix: delete multer

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,18 +6,12 @@ import { UsersService } from './users.service';
 import { UsersRepository } from './repository/user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
-import { MulterModule } from '@nestjs/platform-express';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UsersEntity]),
     forwardRef(() => AuthModule),
     JwtModule,
-    MulterModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-    }),
   ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
