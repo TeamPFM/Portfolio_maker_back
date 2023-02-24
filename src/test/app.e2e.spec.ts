b66dbebcfcce27e0fa5b15afb9cad98cfa784da4
cslee <cslee0034@gmail.com>
2023-02-24 16:36:12 +0900

    fix: rollback

--- src/test/app.e2e.spec.ts
+++ src/test/app.e2e.spec.ts
@@ -19,6 +19,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/api')
       .expect(200)
-      .expect('Hello World!!!');
+      .expect('Hello World!!!!');
   });
 });
