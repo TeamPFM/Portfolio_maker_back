c354962f8a3f1ffda542bbfeb231bfdccf55c648
박주현 <leehot2665@naver.com>
2023-02-27 12:19:56 +0900

    refactor: main에 있는 logger를 파일로 분리

--- src/main.ts
+++ src/main.ts
@@ -2,44 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-import { utilities, WinstonModule } from 'nest-winston';
-import * as winstonDaily from 'winston-daily-rotate-file';
-import * as winston from 'winston';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
-
-const logDir = __dirname + '../../logs';
-
-const dailyOptions = (level: string) => {
-  return {
-    level,
-    datePattern: 'YYYY-MM-DD',
-    dirname: logDir + `/${level}`,
-    filename: `%DATE%.${level}.log`,
-    maxFiles: 30, //30일치 로그파일 저장
-    zippedArchive: true, // 로그가 쌓이면 압축하여 관리
-  };
-};
+import { winstonLogger } from './common/utils/logger.winston';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: WinstonModule.createLogger({
-      transports: [
-        new winston.transports.Console({
-          level: process.env.NODE_ENV === 'prod' ? 'info' : 'silly',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            utilities.format.nestLike('PFM', {
-              prettyPrint: true,
-            }),
-          ),
-        }),
-        new winstonDaily(dailyOptions('info')),
-        new winstonDaily(dailyOptions('warn')),
-        new winstonDaily(dailyOptions('error')),
-      ],
-    }),
+    logger: winstonLogger,
   });
   app.useStaticAssets(path.join(__dirname, '../', 'uploads'), {
     prefix: '/img',
